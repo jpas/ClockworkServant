3b60b38401494982fafbb5fadaee794c86ad1320
jpas <jarrodpas@gmail.com>
2015-05-24 20:48:25 -0600

    add srd feat support

---
+++ handlers/srd/__init__.py
@@ -0,0 +1,50 @@
+from os import listdir, path
+import re
+
+_srd_path = path.dirname(path.abspath(__file__))
+
+class SRD:
+	def __init__(self, folder_name):
+		self._path = '{}/{}/'.format(_srd_path, folder_name)
+		self._db = set()
+		for file in listdir(self._path):
+			if path.isfile(path.join(self._path, file)):
+				self._db.add(str(file[:-3]))
+
+	def __str__(self):
+		return str(self.db)
+
+	def get(self, id):
+		if id in self._db:
+			file_path = path.join(self._path, id + '.md')
+			with open(file_path, 'r') as file:
+				return file.read()
+
+		return None
+
+_dbs = {
+	'feat': SRD('featdb')
+}
+
+def is_request(request):
+	for db in _dbs:
+		if request.lower().startswith('srd {} '.format(db)):
+			return True
+
+	return False
+
+def handle(spec):
+	if is_request(spec):
+		spec = spec[4:].lower()
+	else:
+		return None
+
+	for db_name, db in _dbs.iteritems():
+		if spec.startswith(db_name):
+			id = re.sub(' ', '-', spec[len(db_name)+1:])
+			return db.get(id)
+
+
+if __name__ == '__main__':
+	import sys
+	print handle(' '.join(sys.argv[1:]))
---
+++ handlers/srd/featdb/aberrant-tumor.md
@@ -0,0 +1,12 @@
+# Feat: Aberrant Tumor
+
+_To the surprise of others, that strange growth on you is actually your spellcasting companion._
+
+**Prerequisite**: Aberrant bloodline.
+
+**Benefit**: You gain a tumor familiar, as the tumor familiar alchemist discovery, with an effective alchemist level equal to the level of the class that grants your aberrant bloodline for determining the tumor familiar’s abilities. If multiple classes grant you the aberrant bloodline, those class levels stack for determining your effective alchemist level.
+
+^(Source: [Advanced Class Guide][s-abberant-tumor-f] pg. 136 | Pathfinder Society Legal | [Archives of Nethys][n-abberant-tumor-f])
+
+[s-abberant-tumor-f]: http://paizo.com/products/btpy978v
+[n-abberant-tumor-f]: http://www.archivesofnethys.com/FeatDisplay.aspx?ItemName=Aberrant%20Tumor
