500a5f69c5ef7d1298d0327f939467c449b5da7e
jpas <jarrodpas@gmail.com>
2015-05-24 19:39:18 -0600

    use set for postdb instead of dict

--- postdb/__init__.py
+++ postdb/__init__.py
@@ -1,17 +1,17 @@
 from os.path import abspath, dirname
 
 _db_path = dirname(abspath(__file__)) + '/db.txt'
-_db = {}
+_db = set()
 
 with open(_db_path, 'r') as db:
 	for post_id in db:
-		_db[post_id[:-1]] = True
+		_db.add(post_id)
 
 def has(id):
 	return id in _db
 
 def add(id):
-	if not has(id):
-		_db[id] = True
+	if id not in _db
+		_db.add(id)
 		with open(_db_path, 'a') as db:
 			db.write(id + '\n')
